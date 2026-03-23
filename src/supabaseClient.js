03905b6d8857205fad7dd7ecc6e54f18168146be
elkhaloq <tytyort10@gmail.com>
2026-03-23 08:40:02 +0200

    Delete src/supabaseClient.js

--- src/supabaseClient.js
+++
@@ -1,5 +0,0 @@
-import { createClient } from '@supabase/supabase-api'
-
-const supabaseUrl = 'https://YOUR_PROJECT_URL.supabase.co'
-const supabaseKey = 'YOUR_ANON_KEY'
-export const supabase = createClient(supabaseUrl, supabaseKey)
